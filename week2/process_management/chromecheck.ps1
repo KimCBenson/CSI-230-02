7f74e3d6dfe2c79a62618682f56ed8856ff42bd3
Kimberly Benson <kimberly.benson@mymail.champlain.edu>
2024-09-10 20:40:01 -0400

    Uploaded files for week 2.

---
+++ week2/process_management/chromecheck.ps1
@@ -0,0 +1,14 @@
+﻿# chrome
+$chrome = Get-Process chrome -ErrorAction SilentlyContinue
+
+# if chrome is running, kill it
+if ($chrome)
+{
+    $chrome.Kill()
+}
+
+# if no chrome running, open it and direct to champlain.edu
+else
+{
+    Start-Process "chrome" "https://champlain.edu"
+}
---
+++ week2/process_management/nosystem32processlist.ps1
@@ -0,0 +1,4 @@
+﻿Get-Process | Where-Object {
+    $process_path = (Get-WmiObject Win32_Process -Filter "ProcessId=$($_.Id)").ExecutablePath
+    $process_path -notmatch "system32"
+} | Select-Object Name, Id, @{Name='Path';Expression={$process_path}}

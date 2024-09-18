517e210bccdb108bfeeb9da961d3998f843dbe3f
Kimberly Benson <kimberly.benson@mymail.champlain.edu>
2024-09-17 20:07:40 -0400

    Added week 3 assignments

---
+++ week3/functionsandeventlogs.ps1
@@ -0,0 +1,63 @@
+﻿function getLoginoutsLogs($days)
+{
+# 1: Get-EventLog system -source Microsoft-Windows-Winlogon
+
+# 2:
+    $loginouts = Get-EventLog system -source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-$days)
+    
+    $loginoutsTable = @()
+
+    for ($i=0; $i -lt $loginouts.Count; $i++)
+    {
+        $event = ""
+
+        if ($loginouts[$i].InstanceID -eq 7001) {$event = "Logon"}
+        if ($loginouts[$i].InstanceID -eq 7002) {$event = "Logoff"}
+
+        $user = $loginouts[$i].ReplacementStrings[1]
+
+        $loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated; `
+                                            "Id" = $loginouts[$i].InstanceId; `
+                                            "Event" = $event; `
+                                            "User" = $user;
+                                            }
+    }
+
+    # $loginoutsTable
+
+    # 3:
+    for ($i=0; $i -lt $loginoutsTable.Count; $i++)
+    {
+        $SID = New-Object System.Security.Principal.SecurityIdentifier($loginoutsTable[$i].User)
+
+        $loginoutsTable[$i].User = $SID.Translate([System.Security.Principal.NTAccount])
+    }
+
+    $loginoutsTable
+}
+
+function getStartupShutdownLogs($days)
+{
+    $startups = Get-WinEvent -LogName System | Where-Object { $_.Id -eq 6005 -or $_.Id -eq 6006}
+
+    $startupsTable = @()
+
+    for ($i=0; $i -lt $startups.Count; $i++)
+    {
+        $event = ""
+
+        if ($startups[$i].ID -eq 6005) {$event = "Startup"}
+        if ($startups[$i].ID -eq 6006) {$event = "Shutdown"}
+
+        
+
+        $startupsTable += [pscustomobject]@{"Time" = $startups[$i].TimeCreated; `
+                                            "Id" = $startups[$i].Id; `
+                                            "Event" = $event; `
+                                            "User" = "System";
+                                            }
+    }
+
+    $startupsTable
+}
+
---
+++ week3/functiontest.ps1
@@ -0,0 +1,11 @@
+﻿. (Join-Path $PSScriptRoot functionsandeventlogs.ps1)
+
+# get logins and logoffs from last 15 days
+$loginoutsTable = getLoginoutsLogs(10)
+
+$loginoutsTable
+
+# get startups and shutdowns from last 25 days
+$startupshutdownTable = getStartupShutdownLogs(25)
+
+$startupshutdownTable

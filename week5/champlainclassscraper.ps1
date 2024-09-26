e5eed430f2a500e6b9ae5689efa99db9eaf0519f
Kimberly Benson <kimberly.benson@mymail.champlain.edu>
2024-09-26 13:01:06 -0400

    Added week 5 files for web scraping

---
+++ week5/champlainclassscraper.ps1
@@ -0,0 +1,50 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.13/Courses-1.html
+
+    $trs = $page.ParsedHTML.body.getElementsByTagName("tr")
+
+
+    $FullTable = @()
+
+    for ($i = 1; $i -lt $trs.length; $i++){
+    $tds = $trs[$i].getElementsByTagName("td")
+
+    $Times = $tds[5].innerText.Split("-")
+
+    $FullTable +=  [pscustomobject]@{"Class Code" = $tds[0].outerText;
+                                     "Title" = $tds[1].outerText;
+                                     "Days" = $tds[4].outerText;
+                                     "Time Start" = $Times[0];
+                                     "Time End" = $Times[1];
+                                     "Instructor" = $tds[6].outerText;
+                                     "Location" = $tds[9].outerText;
+                                     }
+
+    }
+
+    daysTranslator $FullTable
+
+    return $FullTable
+}
+
+function daysTranslator($FullTable){
+    for ($i = 0; $i -lt $FullTable.length; $i++){
+        $Days = @()
+
+        if($FullTable[$i].Days -ilike "M"){ $Days += "Monday" }
+
+        if($FullTable[$i].Days -ilike "*[T]*"){ $Days += "Tuesday" }
+        ElseIf($FullTable[$i].Days -ilike "*[T]*"){ $Days += "Tuesday" }
+
+        if($FullTable[$i].Days -ilike "W"){ $Days += "Wednesday" }
+
+        if($FullTable[$i].Days -ilike "Th"){ $Days += "Thursday" }
+
+        if($FullTable[$i].Days -ilike "F"){ $Days += "Friday" }
+
+        $FullTable[$i].Days = $Days
+
+    }
+
+    return $FullTable
+}
---
+++ week5/scrapertest.ps1
@@ -0,0 +1,17 @@
+﻿. (Join-Path $PSScriptRoot champlainclassscraper.ps1)
+
+$FullTable = gatherClasses
+
+# $FullTable | Select "Class Code", Instructor, Days, "Time Start", "Time End" | where {$_."Instructor" -ilike "Furkan Paligu"}
+
+# $FullTable |  Where { ($_.Location -ilike "JOYC 310") -and ($_.days -ilike "Monday")} | Sort-Object "Time Start" | Select "Time Start", "Time End", "Class Code"
+
+
+$ITSInstructors = $FullTable | Where { ($_."Class Code" -ilike "SYS*") -or `
+                                       ($_."Class Code" -ilike "NET*") -or `
+                                       ($_."Class Code" -ilike "SEC*") -or `
+                                       ($_."Class Code" -ilike "FOR*") -or `
+                                       ($_."Class Code" -ilike "CSI*") -or `
+                                       ($_."Class Code" -ilike "DAT*") } | Select "Instructor" | Sort-Object "Instructor" -Unique
+                                        
+$FullTable | Where { $_.Instructor -in $ITSInstructors.Instructor } | Group "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending

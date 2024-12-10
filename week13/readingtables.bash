dd85edbb2876c6a3830fc7a3d1298e087fb8afca
root <kimberly.benson@mymail.champlain.edu>
2024-12-09 19:51:57 -0500

    Reattempted reading tables

--- week13/readingtables.bash
+++ week13/readingtables.bash
@@ -1,15 +1,28 @@
-#!/bin/bash
+#! /bin/bash
 
-link="http://10.0.17.30\Assignment.html"
+link="http://10.0.17.30/Assignment.html"
 
 press=$(curl -sL "$link" | \
 #xmlstarlet format --html --recover 2>/dev/null | \
 xmlstarlet select --template --value-of "//table[@id='press']//tr//td" | \
+awk ' NR % 2 == 1')
+
+temp=$(curl -sL "$link" | \
+xmlstarlet select --template --value-of "//table[@id='temp']//tr//td" | \
 awk 'NR % 2 == 1')
 
-cnt=$(echo "$press" | wc -l)
-for (( i=1; i<="${cnt}"; i++ ))
+dat=$(curl -sL "$link" | \
+xml starlet select --template --value-of "//table[@id='press']//tr//td" | \
+awk 'NR % 2 == 0')
+
+cnt=$(echo "$press" | wc -1)
+for ((  i=1; i<="${cnt}"; i++ ))
 do
-var1=$(echo "$press" | head -n $i | tail -n 1)
+var1=$(echo "$press" | head -n $i  | tail -n 1)
+var2=$(echo "$temp" | head -n $i | tail -n 1)
+var3=$(echo "$dat" | head -n $i | tail -n 1)
 done
-#echo "$press" 
+
+#echo "$press"
+#echo "$temp"
+#echo "$dat"
